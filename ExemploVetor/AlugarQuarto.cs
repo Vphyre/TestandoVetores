c556bfc2ff45da006e9b940da1756e4412840b55
VPhyre <vinipaulnic@hotmail.com>
2018-10-12 23:56:38 -0300

    primeiro envio

---
+++ ExemploVetor/AlugarQuarto.cs
@@ -0,0 +1,30 @@
+﻿
+namespace ExemploVetor
+{
+    class AlugarQuarto
+    {
+        public string nome { get; private set; }
+        public string email { get; private set; }
+
+        //contrutor
+        public AlugarQuarto(string nome, string email)
+        {
+            this.nome = nome;
+            this.email = email;
+        }
+
+        /*  //sobrecarga
+          public AlugarQuarto()
+          {
+              this.nome = " ";
+              this.email = " ";
+          }*/
+
+        //para imprimir
+
+        public override string ToString()
+        {
+            return nome + ", " + email;
+        }
+    }
+}
---
+++ ExemploVetor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace ExemploVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlugarQuarto[] quartos = new AlugarQuarto[10]; 
+            Console.WriteLine("Quantos quartos serão resgistrados? ");
+            
+            int qtd = int.Parse(Console.ReadLine());
+
+
+            if (qtd < 10)
+            {
+
+                for (int i = 0; i < qtd; i++)
+                {
+                    Console.WriteLine("Dados do " + (i + 1) + "° aluguel: ");
+                    Console.WriteLine("Digite seu nome: ");
+                    string nome = Console.ReadLine();
+                    Console.WriteLine("Digite seu email: ");
+                    string email = Console.ReadLine();
+                    quartos[i] = new AlugarQuarto(nome, email);
+                }
+                    Console.WriteLine("Quartos ocupados: ");
+                for (int n = 0; n < qtd; n++)
+                {
+                    Console.WriteLine((n + 1) + "º Quarto: " + quartos[n]);
+                }
+            Console.ReadKey();
+            }
+           
+        }
+    }
+}
